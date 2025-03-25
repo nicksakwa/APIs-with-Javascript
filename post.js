1e64ee62f9ef9379d1e00c18d6ed01fa25c0ed92
nsakwa <nsakwa@gmail.com>
2025-03-26 00:43:14 +0300

    post API

---
+++ post.js
@@ -0,0 +1,30 @@
+document.getElementById('DOMContentLoaded' ,()=>{
+    const postDataButton = document.getElementById('getdata');
+    const postResult=document.getElementById('postresult');
+
+    postDataButton.addEventListener('click', ()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'   
+            },
+            body: JSON,stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            })
+        })
+            .then(response =>{
+                if(!response.ok){
+                    throw new Error('HTTP error! status: ${response.status}');
+                }
+                return response.json();
+            })
+            .then(data =>{
+                postResult.textContent = JSON.stringify(data, null, 2)
+            })
+            .catch(error=>{
+                postResult.textContent='Error: ${error.message}';
+            });
+    });
+});
